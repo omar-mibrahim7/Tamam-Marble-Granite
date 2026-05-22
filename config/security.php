b00a5e3fd0cdea69c0fbf2645cf632912a8dc759
omar-mibrahim7 <omar.mohamed234@msa.edu.eg>
2026-05-22 15:14:53 +0300

    Remove API keys

--- config/security.php
+++ config/security.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('RECAPTCHA_SITE_KEY')) {
-    define('RECAPTCHA_SITE_KEY', '6Le2Qu8sAAAAAEpNEHLsDTUC97S-Tm5tYNmEf6jJ');
+    define('RECAPTCHA_SITE_KEY', 'API KEYS');
 }
 
 if (!defined('RECAPTCHA_SECRET_KEY')) {
-    define('RECAPTCHA_SECRET_KEY', '6Le2Qu8sAAAAAAkscU7ArDO0OF4jkvKwIpg0t20D');
+    define('RECAPTCHA_SECRET_KEY', 'API KEYS');
 }
 ?>
